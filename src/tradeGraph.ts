81e8c7c96471a36f81dd3c93014819349625ad6a
abigpotostew <stew.bracken@gmail.com>
2020-12-10 22:28:39 -0800

    fix yarn build

--- src/tradeGraph.ts
+++ src/tradeGraph.ts
@@ -1,5 +1,5 @@
 import {Trade} from "./store";
-import {Currencies, Currency} from "./marketdata/provider";
+import {Currencies, Currency, UncheckedCurrency} from "./marketdata/provider";
 
 export interface TradeGraph {
 
@@ -17,12 +17,12 @@ interface Node {
     children: Node[]
     parent: Node[] | undefined
 
-    feeValue:number
-    feeCurrency:Currency
-    trade:Trade
+    feeValue: number
+    feeCurrency: Currency
+    trade: Trade
 }
 
-const currencyNone={id:"<NA>", name:"<NA>", symbol:"<NA>"}
+const currencyNone: Currency = new UncheckedCurrency("<NA>")
 
 export function graph(tradesInOrder: Trade[], currencies: Currencies): TradeGraph {
 
@@ -40,98 +40,98 @@ export function graph(tradesInOrder: Trade[], currencies: Currencies): TradeGrap
     // let head :Node
 
 
-    let head:Node = {
+    let head: Node = {
         id: tradesInOrder[0].url + outnode,
-        value: tradesInOrder[0].amount,
-        currency: getReq(tradesInOrder[0].currency),
+        value: tradesInOrder[0].amount.amount,
+        currency: getReq(tradesInOrder[0].amount.currency.symbol),
         children: [],
         parent: undefined,
-        feeValue:tradesInOrder[0].fee,
-        feeCurrency:getReq(tradesInOrder[0].feeCoin),
+        feeValue: tradesInOrder[0].fee.amount,
+        feeCurrency: getReq(tradesInOrder[0].fee.currency.symbol),
         trade: tradesInOrder[0],
     }
-    let leafNodes :Node[]= [];
+    let leafNodes: Node[] = [];
 
 
-    const findLeaf=(forCurrency:Currency, inExchange:string):Node=>{
+    const findLeaf = (forCurrency: Currency, inExchange: string): Node => {
         //for each leaf, find first in exchange and has enough currency same type
         //todo if not enough currency, find all nodes in exchange
         return head
     }
 
-    const calcTotal=(currency:Currency):number=>{
-        return leafNodes.filter((n)=> n.currency.id===currency.id).reduce((prev, curr)=> prev+curr.value, 0)
+    const calcTotal = (currency: Currency): number => {
+        return leafNodes.filter((n) => n.currency.id === currency.id).reduce((prev, curr) => prev + curr.value, 0)
     }
 
-    const totals = new Map<Currency,number>()
+    const totals = new Map<Currency, number>()
 
 
     for (var i = 0; i < tradesInOrder.length; ++i) {
         const trade = tradesInOrder[i]
 
         //find parent node here
-        if (trade.currency==="USD"){
+        if (trade.amount.currency.symbol === "USD") {
             //it's a new usd head
             //add to heads
-        }else {
+        } else {
             //try to find parent leaf for this trade's exchange,
             //todo add exchange to trades
 
         }
 
-        let parentOutNode:Node
+        let parentOutNode: Node
 
         // find parent of out node
-        if (trade.currency=="USD"){
+        if (trade.amount.currency.symbol === "USD") {
             parentOutNode = {
                 id: trade.url + outnode,
-                value: trade.amount,
-                currency: getReq(trade.currency),
+                value: trade.amount.amount,
+                currency: getReq(trade.amount.currency.symbol),
                 children: [],
                 parent: undefined,
-                feeValue:trade.fee,
-                feeCurrency:getReq(trade.feeCoin),
+                feeValue: trade.fee.amount,
+                feeCurrency: getReq(trade.fee.currency.symbol),
                 trade: trade,
             }
-        }else{
+        } else {
             //find parent from leaves for our currency
             //which tree?
-            parentOutNode =findLeaf(getReq(trade.currency), trade.exchange)
+            parentOutNode = findLeaf(getReq(trade.amount.currency.symbol), trade.exchange)
             //todo what if it's not found?
         }
 
 
-        const inNode:Node = {
+        const inNode: Node = {
             id: trade.url + innode,
-            value: trade.inAmount,
-            currency: getReq(trade.inCurrency),
+            value: trade.amount.amount,
+            currency: getReq(trade.amount.currency.symbol),
             children: [],
             parent: [parentOutNode],
-            feeValue:0,
-            feeCurrency:currencyNone,
-            trade:trade
+            feeValue: 0,
+            feeCurrency: currencyNone,
+            trade: trade
         }
         parentOutNode.children.push(inNode)
         //calc remainder
         /**
          * for each leaf
          */
-        let totalOut = calcTotal(outNode.currency)
-        if(totalOut - trade.amount > 0){
-
-            const remainderNode:Node = {
-                id: trade.url + remaindernode,
-                value: totalOut - trade.amount,
-                currency: getReq(trade.currency),
-                children: [],
-                parent: [outNode],
-                feeValue:0,
-                feeCurrency:currencyNone,
-                trade:trade
-            }
-
-            outNode.children.push(inNode)
-        }
+        // let totalOut = calcTotal(outNode.currency)
+        // if (totalOut - trade.amount > 0) {
+        //
+        //     const remainderNode: Node = {
+        //         id: trade.url + remaindernode,
+        //         value: totalOut - trade.amount,
+        //         currency: getReq(trade.currency),
+        //         children: [],
+        //         parent: [outNode],
+        //         feeValue: 0,
+        //         feeCurrency: currencyNone,
+        //         trade: trade
+        //     }
+        //
+        //     outNode.children.push(inNode)
+        // }
     }
 
     return {}
